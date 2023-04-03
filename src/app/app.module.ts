0dacd212f15062fbbf26f3127e65219980f1ae50
lenny1596 <leninbose1596@gmail.com>
2023-04-03 23:17:17 +0530

    Final Commit, adding about, footer & main comp to router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // components
 import { AppComponent } from './app.component';
@@ -12,6 +13,14 @@ import { ButtonComponent } from './components/button/button.component';
 import { NoteItemComponent } from './components/note-item/note-item.component';
 import { NotesComponent } from './components/notes/notes.component';
 import { AddNoteComponent } from './components/add-note/add-note.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+// Angular Routing
+const addRoutes: Routes = [
+  { path: '', component: NotesComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +30,16 @@ import { AddNoteComponent } from './components/add-note/add-note.component';
     NoteItemComponent,
     NotesComponent,
     AddNoteComponent,
+    AboutComponent,
+    FooterComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(addRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/add-note/add-note.component.ts
+++ src/app/components/add-note/add-note.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NoteType } from 'src/app/NoteType';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-note',
@@ -11,6 +13,14 @@ export class AddNoteComponent implements OnInit {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddNote?: boolean;
+  subscription?: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddNote = value));
+  }
 
   ngOnInit(): void {}
 
