ba962f4090ca8ed5a843cb51182879f9267ed4d7
lenny1596 <leninbose1596@gmail.com>
2023-04-03 22:23:05 +0530

    adding func. to add tgl btn & adding nw service ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'My Notes';
+  showAddNote?: boolean;
+  subscription?: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddNote = value));
+  }
 
   ngOnInit(): void {}
 
-  toggleAddTask(): void {
-    console.log('toggle');
+  toggleAddNote(): void {
+    this.uiService.toggleAddNote();
   }
 }
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -3,33 +3,38 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { NoteType } from '../NoteType';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type': 'application/json',
-  }),
-};
-
 @Injectable({
   providedIn: 'root',
 })
 export class NoteService {
+  // this link refers to the json local db created that allows crud ops.
   private apiUrl = 'http://localhost:5000/notes';
 
-  constructor(private http: HttpClient) {}
+  // these http headers are provided to be used with post, put & patch ops
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
+  constructor(private http: HttpClient) {}
+  // find all notes and display them
   getNotes(): Observable<NoteType[]> {
     return this.http.get<NoteType[]>(this.apiUrl);
   }
 
+  // add new notes to db
   addNotes(note: NoteType): Observable<NoteType> {
-    return this.http.post<NoteType>(this.apiUrl, note, httpOptions);
+    return this.http.post<NoteType>(this.apiUrl, note, this.httpOptions);
   }
 
+  // update the reminder's boolean values
   updateReminders(note: NoteType): Observable<NoteType> {
     const url = `${this.apiUrl}/${note.id}`;
-    return this.http.put<NoteType>(url, note, httpOptions);
+    return this.http.put<NoteType>(url, note, this.httpOptions);
   }
 
+  // delete a note from db
   deleteNotes(note: NoteType): Observable<NoteType> {
     const url = `${this.apiUrl}/${note.id}`;
     return this.http.delete<NoteType>(url);
